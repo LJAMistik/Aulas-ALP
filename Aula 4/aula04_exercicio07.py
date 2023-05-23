cb8c9cfe2eca5c8e5fd58400b4d800cad162c811
Laura Jane Antunes <laura.antunes@gmail.com>
2023-05-23 15:24:26 -0300

    atualizado 3 exercicios

--- Aula 4/aula04_exercicio07.py
+++ Aula 4/aula04_exercicio07.py
@@ -3,3 +3,15 @@ um depósito e o valor da taxa de juros,
 calcule e mostre o valor do rendimento e
 o valor total depois do rendimento.
 """
+# Pedir ao usuário:
+deposito = float(input("Digite o valor que deseja depositar em R$: "))
+juros = float(input("Digite o valor percentual da taxa de juros: "))
+
+# Calcular
+juros_conv = deposito * juros // 100
+rendimento = juros_conv
+total_rendimentos = deposito + juros_conv
+
+#Mostrar os valores:
+print(f"O valor do rendimento dos juros é de R$ {rendimento:.2f}")
+print(f"O valor total do seu rendimento é de R$ {total_rendimentos:.2f}")
--- Aula 4/aula04_exercicio08.py
+++ Aula 4/aula04_exercicio08.py
@@ -2,3 +2,14 @@
 área de um triângulo. Sabe-se que Área =
 (base * altura)/2
 """
+
+# Pedir ao usuário os valores de base e altura do triangulo:
+altura = float(input("Digite o valor de altura deste triângulo: "))
+base = float(input("Digite o valor da base deste triângulo: "))
+
+# Calcular:
+area_triangulo = base * altura // 2
+
+# Mostrar o resultado do calculo:
+
+print("O resultado é: ", area_triangulo)
