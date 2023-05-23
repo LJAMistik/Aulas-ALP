4cb02bd71c4ecad94952e19a2347546fb355b8aa
Laura Jane Antunes <laura.antunes@gmail.com>
2023-05-23 19:29:52 -0300

    atualizei denovo

--- Aula 4/aula04_exercicio09.py
+++ Aula 4/aula04_exercicio09.py
@@ -4,3 +4,12 @@ Area= π * R2
 π = 3,1415
 """
 
+# Pedir ao usuario o valor do raio
+raio_circulo = float(input("Digite o raio do círculo: "))
+
+# Calcular a formula:
+Area = 3.1415 * (raio_circulo**2)
+
+# Mostrar o resultado:
+print(f"A Area do circulo é: {Area:.2f}")
+
